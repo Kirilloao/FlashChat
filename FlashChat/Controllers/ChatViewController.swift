ef1bae3fe1b0c72d13395ce7539c3839fa82ab55
Kirilloao <kirilltaraturin747@gmail>
2023-09-13 11:51:40 +0200

    create chatViewController and chatView

---
+++ FlashChat/Controllers/ChatViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ChatViewController.swift
+//  FlashChat
+//
+//  Created by Kirill Taraturin on 13.09.2023.
+//
+
+import UIKit
+
+final class ChatViewController: UIViewController {
+
+    // MARK: - Life Cycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK: - Private Methods
+    private func setupConstraints() {
+        
+    }
+
+}
---
+++ FlashChat/Views/ChatView.swift
@@ -0,0 +1,20 @@
+//
+//  ChatView.swift
+//  FlashChat
+//
+//  Created by Kirill Taraturin on 13.09.2023.
+//
+
+import UIKit
+
+class ChatView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
